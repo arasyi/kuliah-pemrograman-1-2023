f4ae9ec2b84393ce957f4c6bb2317de0ad4708df
Mohammad Arif Rasyidi <arasyi@gmail.com>
2023-03-14 08:33:36 +0700

    Kuliah Minggu 1

---
+++ 01/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!"); // baris 1
+        System.out.println("Hello, UISI!"); // baris 1 juga
+        System.out.println("abc\n\\\t\"");
+    }    
+}
